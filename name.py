dfc99b73c456261872306d0873bf5078947a3418
yousef-moustafa <ymoustafa005@gmail.com>
2024-06-04 22:37:29 +0300

    resolved conflicts

---
+++ name.py
@@ -0,0 +1,18 @@
+class Hossam:
+    def __init__(self):
+        self.l = "Enta gamed awy"
+    def gamed(self):
+        return self.l
+
+Mohamed = Hossam()
+print(Mohamed.gamed())
+
+
+
+
+
+
+
+
+
+print('Hellooooo')
