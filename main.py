fa1aa85c6b909c746d239e3bcdb09b35e6a11595
mohamedhelmy21 <mohamed.helmy@gaf.ac>
2024-06-21 23:53:43 +0300

    zooming in/out
    basic zooming in and zooming out, i added a base of map height and map width, display height and display width functions and parameters for future implementation of max zooming in/out

--- main.py
+++ main.py
@@ -17,13 +17,11 @@ map_path = 'assets/map_data/tmx/map.tmx'
 map_loader = MapLoader(map_path)
 map_loader.load_map()
 sprite_group = map_loader.get_sprite_group()
+map_width = map_loader.get_map_width()
+map_height = map_loader.get_map_height()
 
-# Add player to the map
-#player = pg.sprite.GroupSingle()
-#player.add(Player())
-
-#Camera Group 
-camera_group = CameraGroup()
+#Camera Group / Map
+camera_group = CameraGroup(map_width, map_height, settings.width, settings.height)
 for sprite in sprite_group:
     camera_group.add(sprite)
 
@@ -37,15 +35,17 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-    # Draw screen and add player
-    #screen.fill(settings.background_color)
-    #sprite_group.draw(screen)
-    #player.draw(screen)
-    #player.update()
-    # Update player and other sprites
-    camera_group.update()
+        #zooming with mouse
+        if event.type == pg.MOUSEWHEEL:
+            camera_group.zoom_scale += event.y * 0.03
+            #for max zoom in/out future implementation
+            #camera_group.zoom_scale = max (camera_group.min_zoom_scale, min(camera_group.zoom_scale, camera_group.max_zoom_scale))
 
-    # Draw screen with the camera view
+    # Update player and other sprites
+    player.update()
+    camera_group.center_target_camera(player)
+
+    # Draw screen with the camera view and add the player
     screen.fill(settings.background_color)
     camera_group.custom_draw(player)
     pg.display.flip()
--- src/map_loader.py
+++ src/map_loader.py
@@ -28,8 +28,15 @@ class MapLoader:
     def get_sprite_group(self):
         return self.sprite_group
     
+    #for max map zoom out future implementation
+    def get_map_width(self):
+        return 88 * 16
+    
+    def get_map_height(self):
+        return 96 * 16
+    
 class CameraGroup(pg.sprite.Group):
-    def __init__(self):
+    def __init__(self, map_width, map_height, display_width, display_height):
         super().__init__()
         self.display_surface = pg.display.get_surface()
 
@@ -37,14 +44,46 @@ class CameraGroup(pg.sprite.Group):
         self.offset = pg.math.Vector2()
         self.half_w = self.display_surface.get_size()[0] // 2
         self.half_h = self.display_surface.get_size()[1] // 2
-    
+
+        #zoom
+        self.zoom_scale = 1
+        #for max zoom in/out future implementation
+        self.min_zoom_scale = 0.5
+        self.max_zoom_scale = min(display_width / map_width, display_height / map_height)
+
+        self.internal_surf_size = (2500, 2500)
+        self.internal_surf = pg.Surface(self.internal_surf_size, pg.SRCALPHA)
+        self.internal_rect = self.internal_surf.get_rect(center = (self.half_w, self.half_h))
+        self.internal_surf_size_vector = pg.math.Vector2(self.internal_surf_size)
+        self.internal_offset = pg.math.Vector2()
+        self.internal_offset.x = self.internal_surf_size[0] // 2 - self.half_w
+        self.internal_offset.y = self.internal_surf_size[1] // 2 - self.half_h
+
+    def zoom_keyboard_control(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_q]:
+            self.zoom_scale += 0.1
+        if keys[pg.K_e]:
+            self.zoom_scale -= 0.1
+
     def center_target_camera(self, target):
         self.offset.x = target.rect.centerx - self.half_w
         self.offset.y = target.rect.centery - self.half_h
 
     def custom_draw(self, player):
         self.center_target_camera(player)
+
+        #filling the internal surface
+        self.internal_surf.fill('#47b455')
+
+        self.zoom_keyboard_control()
+
         for sprite in self.sprites():
-            offset_pos = sprite.rect.topleft - self.offset
-            self.display_surface.blit(sprite.image, offset_pos)
+            offset_pos = sprite.rect.topleft - self.offset + self.internal_offset
+            self.internal_surf.blit(sprite.image, offset_pos)
+        
+        scaled_surf = pg.transform.scale(self.internal_surf, self.internal_surf_size_vector * self.zoom_scale)
+        scaled_rect = scaled_surf.get_rect(center = (self.half_w, self.half_h))
+        self.display_surface.blit(scaled_surf, scaled_rect)
+
 
